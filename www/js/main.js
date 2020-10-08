024cab43bda9592c075215d453fd65ea03e45243
Erlliam Mejia <tommejas@gmail.com>
2020-10-08 01:10:01 -0400

    Remove comments in main.js

--- www/js/main.js
+++ www/js/main.js
@@ -2,14 +2,9 @@
 
 (async () => {
   const DATABASE_NAME = "Calorie Tracker";
-
-  // ugly?
   let date = new Date(getDateString(new Date()));
-
   let database = new Database({ name: DATABASE_NAME });
   await database.ready;
-
-  // XXX singleton here?
 
   let header = new Header({
     date: date,
@@ -19,16 +14,12 @@
   });
 
   let overview = new Overview();
+
   let diary = new Diary({
     container: document.getElementById("diary"),
     database: database,
     date: date
   });
-  let diaryOptions = new DiaryOptions({ database: database, date: date });
-  //initializeDiaryOptions(database, date);
 
-  // XXX test
-  // let food = {"foodKey":3,"name":"Aqua lentils","servingSize":58,"unit":"g","calories":102}
-  // let popUp = new PopUp({ database: database, date: date });
-  // popUp.addFood(food);
+  let diaryOptions = new DiaryOptions({ database: database, date: date });
 })();
